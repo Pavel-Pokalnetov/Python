6994589a427d4bd36c07dcf50689dcf333018352
Pokalnetov Pavel <pokalnetovps@gmail.com>
2022-12-16 01:52:09 +0500

    задача 12

--- homework_2_12#2.py
+++ homework_2_12#2.py
@@ -1,35 +1,31 @@
 '''
+вариант 2
 Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике. 
 Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. 
 Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P.  
 Помогите Кате отгадать задуманные Петей числа.  
 '''
 import random
-from math import sqrt
+
 
 def helpKaty(S,P):
-    D = S*S-4*P
-    resultX=0
-    if D<0:
+    resultX=resultY=-1
+    for x in range(0,1001):
+        if (x*x-S*x+P)==0:
+            resultX=x
+            resultY=(int)(P/x)
+            break
+    if resultX==-1:
         print('Петя чего-то напутал, такой пары не может быть')
-    elif D==0:
-        resultX = S/2        
     else:
-        resultX = (S + sqrt(D))/2
-    resultY = S-resultX
-    
-    if (int(resultX))==resultX and (int(resultY))==resultY:
-        print(f'X={int(resultX)} Y={int(resultY)}')    
-    else:
-        print('Петя лукавит!\nцифры-то не целые')
-        print(f'X={resultX:.1f} Y={resultY:.1f}')
-
+        print(f'X={resultX} Y={resultY}') 
 
 
 plan = [(9,20),
         (15,56),
         (31,220),
         (28,52)]
+
 for item in plan:
     S,P = item
     print(f'S={S} P={P}',end='\t')
@@ -42,12 +38,5 @@ X	Y		S	P
 7	8		15	56
 20	11		31	220
 2	26		28	52
-7	46		53	322
-45	12		57	540
-17	29		46	493
-2	3		5	6
-5	6		11	30
-7	8		15	56
-10	20		30	200
 
 '''
